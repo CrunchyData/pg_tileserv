975ca564a4601b27d2e1a8d53111b19f7962a3d5
Paul Ramsey <pramsey@cleverelephant.ca>
2020-01-10 12:16:49 -0800

    drop suite

--- main_test.go
+++ main_test.go
@@ -11,59 +11,41 @@ import (
 	"github.com/jackc/pgx/v4/pgxpool"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/suite"
 )
 
-type dbSuite struct {
-	suite.Suite
-	setup bool
-}
-
 var db *pgxpool.Pool
+var dbsetup = false
 
-func (suite *dbSuite) SetupSuite() {
+func TestMain(m *testing.M) {
+
 	viper.Set("DbConnection", os.Getenv("TEST_DATABASE_URL"))
 	db, err := DbConnect()
 	if err != nil {
-		suite.setup = false
-		return
+		os.Exit(1)
 	}
 	sql := "CREATE EXTENSION IF NOT EXISTS postgis"
 	_, err = db.Exec(context.Background(), sql)
 	if err != nil {
-		suite.T().Skip("DB integration test suite setup failed, skipping")
-		suite.setup = false
-		return
+
+		os.Exit(1)
 	}
-	suite.setup = true
+
+	dbsetup = true
+	os.Exit(m.Run())
 }
 
-func (suite *dbSuite) TearDownSuite() {
-}
-
-func (suite *dbSuite) SetupTest() {
-}
-
-func (suite *dbSuite) TearDownTest() {
-}
-
-func (suite *dbSuite) TestDBNoTables() {
-	if !suite.setup {
-		suite.T().Skip("DB integration test suite setup failed, skipping")
+func TestDBNoTables(t *testing.T) {
+	if !dbsetup {
+		t.Skip("DB integration test suite setup failed, skipping")
 	}
 	r := TileRouter()
 	request, _ := http.NewRequest("GET", "/index.json", nil)
 	response := httptest.NewRecorder()
 	r.ServeHTTP(response, request)
-	assert.Equal(suite.T(), 200, response.Code, "OK response is expected")
+	assert.Equal(t, 200, response.Code, "OK response is expected")
 
 	json_result := response.Body.String()
 	json_result = strings.TrimSpace(json_result)
 	json_expect := "{}"
-	assert.Equal(suite.T(), json_expect, json_result, "empty json response is expected")
-}
-
-func TestDatabaseSuite(t *testing.T) {
-	tests := new(dbSuite)
-	suite.Run(t, tests)
+	assert.Equal(t, json_expect, json_result, "empty json response is expected")
 }
