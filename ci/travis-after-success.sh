ce5eb13de3a58fdd3c17034fa971292dc0b0dbae
Paul Ramsey <pramsey@cleverelephant.ca>
2020-01-23 13:28:10 -0800

    push fewer docker tags

--- ci/travis-after-success.sh
+++ ci/travis-after-success.sh
@@ -15,10 +15,12 @@ fi
 if [ "$TARGET" = "docker" ]; then
     DOCKER_REPO=pramsey/pg_tileserv
     VERSION=`./pg_tileserv --version | cut -f2 -d' '`
+    DATE=`date +%Y%m%d`
     docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
     docker build -f Dockerfile --build-arg VERSION=$VERSION -t $DOCKER_REPO:$TAG .
-    docker tag $DOCKER_REPO:$TAG $DOCKER_REPO:$TRAVIS_COMMIT
-    docker tag $DOCKER_REPO:$TAG $DOCKER_REPO:travis-$TRAVIS_BUILD_NUMBER
+    #docker tag $DOCKER_REPO:$TAG $DOCKER_REPO:$TRAVIS_COMMIT
+    #docker tag $DOCKER_REPO:$TAG $DOCKER_REPO:travis-$TRAVIS_BUILD_NUMBER
+    docker tag $DOCKER_REPO:$TAG $DOCKER_REPO:$DATE
     docker push $DOCKER_REPO
 else
     mkdir upload
