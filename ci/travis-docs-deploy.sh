5de29be16f41447e8e006e8bcd0ba7e4b59e7dca
Paul Ramsey <pramsey@cleverelephant.ca>
2020-02-09 15:39:08 -0800

    travis

--- ci/travis-docs-deploy.sh
+++ ci/travis-docs-deploy.sh
@@ -6,5 +6,7 @@ if [ "$TARGET" = "docs" ]; then
     echo "in docs deploy branch"
     git add -f docs
     git status
+    git commit -m 'travis doc build'
+    git push pramsey $TRAVIS_BRANCH --force
 fi
 
