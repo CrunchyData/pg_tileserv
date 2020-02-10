ae936d5ea77ce2e9e553e791e4b36bc429fbc619
Paul Ramsey <pramsey@cleverelephant.ca>
2020-02-09 20:03:30 -0800

    travis

--- ci/travis-install.sh
+++ ci/travis-install.sh
@@ -7,7 +7,7 @@ fi
 if [ "$TARGET" = "docs" ]; then
     VER=0.64.1
     FILE=hugo_${VER}_Linux-64bit.deb
-	URL=https://github.com/gohugoio/hugo/releases/download/v${VER}/${FILE}
+    URL=https://github.com/gohugoio/hugo/releases/download/v${VER}/${FILE}
     curl -LO $URL
     sudo dpkg -i ${FILE}
 fi
