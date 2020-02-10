8385f6c66b71e015f6b2cd97271e251f86f8b042
Paul Ramsey <pramsey@cleverelephant.ca>
2020-02-09 16:02:18 -0800

    travis

--- ci/travis-docs-deploy.sh
+++ ci/travis-docs-deploy.sh
@@ -3,11 +3,14 @@
 echo "docs deploy ran"
 
 if [ "$TARGET" = "docs" ]; then
-    echo "in docs deploy branch"
+    echo "in docs deploy branch: $TRAVIS_BRANCH"
+
     git add -f docs
     git status
-    git commit -m 'travis doc build'
-    echo branch: $TRAVIS_BRANCH
-    git push docbuild --force
+    git config user.name "Travis CI"
+    git config user.email "travis@travis-ci.org"
+    git commit --message "Auto deploy from Travis CI"
+    git remote add deploy "https://$GITHUB_TOKEN@github.com/pramsey/pg_tileserv"
+    git push deploy docbuild
 fi
 
