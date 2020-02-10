cc26cb07bb299a599ded8b1602ba1fc65c966b83
Paul Ramsey <pramsey@cleverelephant.ca>
2020-02-09 19:45:42 -0800

    travis

--- ci/travis-docs-deploy.sh
+++ ci/travis-docs-deploy.sh
@@ -5,7 +5,7 @@ echo "docs deploy ran"
 if [ "$GITHUB_TOKEN" != "" ]; then
     LOCAL_BRANCH=docs-$TRAVIS_BUILD_NUMBER
     echo "Running documentation deploy script in $TRAVIS_BRANCH"
-    git remote add deploy "https://$GITHUB_TOKEN@github.com/pramsey/pg_tileserv.git"
+    git remote add deploy "https://$GITHUB_TOKEN@github.com/CrunchyData/pg_tileserv.git"
     git checkout -b $LOCAL_BRANCH
     git branch -v
     git add -f docs
