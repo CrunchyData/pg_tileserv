409588653fd95e14f8009b7cf68f1a9519405066
PanierAvide <panieravide@riseup.net>
2020-08-23 15:04:41 +0200

    Add support for materialized views

--- layer_table.go
+++ layer_table.go
@@ -443,7 +443,7 @@ func GetTableLayers() ([]LayerTable, error) {
 	LEFT JOIN pg_index i ON (c.oid = i.indrelid AND i.indisprimary AND i.indnatts = 1)
 	LEFT JOIN pg_attribute ia ON (ia.attrelid = i.indexrelid)
 	LEFT JOIN pg_type it ON (ia.atttypid = it.oid AND it.typname in ('int2', 'int4', 'int8'))
-	WHERE c.relkind IN ('r', 'v')
+	WHERE c.relkind IN ('r', 'v', 'm')
 		AND t.typname = 'geometry'
 		AND has_table_privilege(c.oid, 'select')
 		AND has_schema_privilege(n.oid, 'usage')
